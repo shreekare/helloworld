075e71f5bd28148bf09707a23f7c6c2779361703
Shreekar Enugandla <shreekar@gmail.com>
2021-03-17 17:48:46 +0530

    adding scan seq

--- helloworld.c
+++ helloworld.c
@@ -5,3 +5,28 @@ int main()
     printf("Hello World!\n");
     printf("Hello Shreekar\n");
 }
+
+scan_seq()
+{
+    Chunk *head = NULL, *cur = NULL;
+
+    while (read next number into 'n')
+    {
+        if (cur == NULL || cur->filled == CHUNK_SIZE)
+        {
+            if (head == NULL)
+            {
+                cur = head = (Chunk *) malloc(sizeof(Chunk));
+            }
+            else
+            {
+                cur->next = (Chunk *) malloc(sizeof(Chunk));
+                cur = cur->next;
+            }
+            cur->filled = 0;
+            cur->next = NULL;
+        }
+        cur->data[cur->filled++] = n;
+    }
+    return head;
+}
